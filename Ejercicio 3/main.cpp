68e9c4c851b342ab12e6e51c27acb89b40539f9c
nquinbla <nquinbla@myuax.com>
2023-11-16 13:14:18 +0100

    Ejercicio 3 (final)

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -7,19 +7,25 @@ using namespace geometria;
 int main() {
     // Solicitar al usuario el radio del círculo
     double radio;
-    std::cout << "Ingrese el radio del círculo: ";
+    std::cout << "Ingrese el radio del circulo: ";
     std::cin >> radio;
 
     // Crear un objeto de la clase Circulo
     Circulo miCirculo(radio);
 
     // Calcular y mostrar el área del círculo
+    std::cout << "Presiona Enter para calcular el area del circulo...";
+    std::cin.ignore();
+    std::cin.get();
     double area = miCirculo.calcularArea();
-    std::cout << "Área del círculo: " << area << std::endl;
+    std::cout << "Area del circulo: " << area << std::endl;
 
     // Calcular y mostrar el perímetro del círculo
+    std::cout << "Presiona Enter para calcular el perimetro del circulo...";
+    std::cin.ignore();
+    std::cin.get();
     double perimetro = miCirculo.calcularPerimetro();
-    std::cout << "Perímetro del círculo: " << perimetro << std::endl;
+    std::cout << "Perimetro del circulo: " << perimetro << std::endl;
 
     return 0;
 }
