333d8c318bcb3c57f4978e4ef24a2b2388360dd8
nquinbla <nquinbla@myuax.com>
2023-11-16 13:03:14 +0100

    ejercicio 2 (final)

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,29 +1,42 @@
 // Ejercicio 2: Espacio de nombres repartido entre diferentes archivos Crea un programa donde defines un espacio de nombres "Geometría" en dos archivos de código separados (por ejemplo, "triángulo.cpp" y "círculo.cpp"). Cada archivo debe contener una función (por ejemplo, "calcularArea") correspondiente a la geometría específica. Finalmente, desde la función main en otro archivo, utiliza estas funciones. en c++ en clion con flujo de trabajo de cmake
 
-
 #include <iostream>
 #include "triangulo.h"
 #include "circulo.h"
 
-using namespace std;
+using namespace Geometria;
 
 int main() {
-    double base, altura, radio;
+    double baseTriangulo, alturaTriangulo, radioCirculo;
 
-    cout << "Introduce la base del triangulo: ";
-    cin >> base;
+    // Mensaje inicial
+    std::cout << "Programa compilado con exito. Presiona Enter para continuar...";
+    std::cin.ignore();  // Esperar a que el usuario presione Enter
 
-    cout << "Introduce la altura del triangulo: ";
-    cin >> altura;
+    std::cout << "Ingrese la base del triangulo: ";
+    std::cin >> baseTriangulo;
 
-    cout << "Introduce el radio del circulo: ";
-    cin >> radio;
+    std::cout << "Ingrese la altura del triangulo: ";
+    std::cin >> alturaTriangulo;
 
-    double areaTriangulo = Geometria::calcularAreaTriangulo(base, altura);
-    double areaCirculo = Geometria::calcularAreaCirculo(radio);
+    std::cout << "Presiona Enter para calcular el area del triangulo...";
+    std::cin.ignore();  // Esperar a que el usuario presione Enter
+    std::cin.get();
+    double areaTriangulo = calcularAreaTriangulo(baseTriangulo, alturaTriangulo);
+    std::cout << "Area del triangulo: " << areaTriangulo << std::endl;
 
-    cout << "El area del triangulo es: " << areaTriangulo << endl;
-    cout << "El area del circulo es: " << areaCirculo << endl;
+    std::cout << "Ingrese el radio del circulo: ";
+    std::cin >> radioCirculo;
+
+    std::cout << "Presiona Enter para calcular el area del circulo...";
+    std::cin.ignore();  // Esperar a que el usuario presione Enter
+    std::cin.get();
+    double areaCirculo = calcularAreaCirculo(radioCirculo);
+    std::cout << "Area del circulo: " << areaCirculo << std::endl;
+
+    // Mensaje final
+    std::cout << "Presiona Enter para salir...";
+    std::cin.ignore();  // Esperar a que el usuario presione Enter
 
     return 0;
 }
